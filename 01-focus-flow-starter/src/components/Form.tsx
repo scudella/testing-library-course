8694889304c66efb2f7eab48292bc9b5d8a3e826
Eduardo S. Libardi <eslibardi@gmail.com>
2025-05-14 21:09:28 -0300

    Core Logic

---
+++ 01-focus-flow-starter/src/components/Form.tsx
@@ -0,0 +1,6 @@
+import {type ItemWithoutId, type ItemCategory} from '../utils'
+
+const Form = ({onSubmit}: {onSubmit: (item: ItemWithoutId) => void}) => {
+  return <div>Form</div>
+}
+export default Form
---
+++ 01-focus-flow-starter/src/components/List.tsx
@@ -0,0 +1,14 @@
+import ItemCard from './ItemCard'
+import {type Item} from '../utils'
+
+const List = ({
+  items,
+  onDelete,
+}: {
+  items: Item[]
+  onDelete: (id: string) => void
+}) => {
+  console.log(items, onDelete)
+  return <div>List</div>
+}
+export default List
